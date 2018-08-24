c96df8ac272b1aef6118c9e77eb040fdd23ce425
Adam Vaccaro <avaccaro@geosys.usc.edu>
2018-08-24 14:17:15 -0700

    Remove obsolete script

--- slurm/RUN_generate_spars_adj_range.m
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -f
-#SBATCH --job-name=gen_spars_adjs
-#SBATCH --output=gen_spars_adjs.o
-#SBATCH --error=gen_spars_adjs.e
-cd /home/geovault-02/avaccaro/hadCRUT4.6/
-/usr/bin/matlab -nodesktop -nosplash > ./out/generate_spars_adj_range.out << EOF
-generate_spars_adj_range
-exit
-EOF
