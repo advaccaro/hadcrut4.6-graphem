7e8453756ee009e1477dd25f1341d5ae39c079e1
advaccaro <avaccaro@usc.edu>
2019-05-09 19:49:58 -0700

    Update directory paths

--- load_gmt_datasets.m
+++ load_gmt_datasets.m
@@ -1,23 +1,23 @@
 %load GMT datasets
 
 %HadCRUT4.6 raw explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/data/H46MED_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46MED_EXP.mat');
 
 %HadCRUT4.6 graphem infilled (sparsity 0.8%) explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/data/H46M80_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46M80_EXP.mat');
 
-%HadCRUT4.6 graphem infilled (sparsity 0.8%) explored
+%HadCRUT4.6 graphem infilled (sparsity 0.6%) explored
 load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46MED_SP60_MERRA_KRIG_EXP.mat');
 
 
 %GISTEMP explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/GISTEMP/data/GISTEMP_GMT.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/GISTEMP/data/GISTEMP_GMT.mat');
 
 %Cowtan & Way explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/data/C46MED_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/C46MED_EXP.mat');
 
 %NOAA explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/NOAA_GMT/data/NOAA_GMT_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/NOAA_GMT/data/NOAA_GMT_EXP.mat');
 
 %GraphEM ensemble
 load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/ensemble/data/ENS_sp60.mat');
