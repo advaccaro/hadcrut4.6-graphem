644cc245b5abce99c5980d89a11eeb7972608483
advaccaro <avaccaro@usc.edu>
2020-07-01 14:14:01 -0700

    Update paths to data files

--- load_nino_datasets.m
+++ load_nino_datasets.m
@@ -5,25 +5,25 @@ load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46MED_EXP.mat');
 
 
 %HadCRUT4.6 graphem infilled (sparsity 0.8%) explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/data/H46MED_SP80_MERRA_KRIG_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46MED_SP80_MERRA_KRIG_EXP.mat');
 
-%HadCRUT4.6 graphem infilled (sparsity 0.8%) explored
+%HadCRUT4.6 graphem infilled (sparsity 0.6%) explored
 load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/H46MED_SP60_MERRA_KRIG_EXP.mat');
 
 %Kaplan ext explored
-load('/home/scec-02/avaccaro/HadCRUT4.3/SST_datasets/KAPLANext/data/KAPLANext_NINO.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/SST_datasets/KAPLANext/data/KAPLANext_NINO.mat');
 
 %COBE
-load('/home/geovault-02/avaccaro/hadCRUT4.6/SST_datasets/cobe/COBE_NINO.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/SST_datasets/cobe/COBE_NINO.mat');
 
 %Bunge & Clarke
-load('/home/scec-02/avaccaro/HadCRUT4.3/SST_datasets/Bunge/data/BUNGE_NINO.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/SST_datasets/Bunge/data/BUNGE_NINO.mat');
 
 %ERSSTv5
-load('/home/geovault-02/avaccaro/hadCRUT4.6/SST_datasets/ERSSTv5/data/ERSSTv5_NINO.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/SST_datasets/ERSSTv5/data/ERSSTv5_NINO.mat');
 
 %Cowtan & Way explored
-load('/home/geovault-02/avaccaro/hadCRUT4.6/data/C46MED_EXP.mat');
+load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/data/C46MED_EXP.mat');
 
 %GraphEM ensemble
 load('/home/geovault-02/avaccaro/hadcrut4.6-graphem/ensemble/data/ENS_sp60.mat');
