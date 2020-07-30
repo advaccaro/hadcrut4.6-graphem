d647d6c9ea94709cfbf0fb70171f920de08cd531
advaccaro <avaccaro@usc.edu>
2020-07-29 23:27:20 -0700

    Update combine script

--- CCI/cci_combine_netcdf.m
+++ CCI/cci_combine_netcdf.m
@@ -1,5 +1,14 @@
 % combine_netcdf.m
-function CCI = combine_netcdf(indir)
+function CCI = combine_netcdf(indir, ofile, odir)
+	% Check for optional arguments
+	if ~exist('odir', 'var')
+		odir = '/home/geovault-02/avaccaro/hadcrut4.6-graphem/CCI/data/'
+	end
+
+	if ~exist('ofile', 'var')
+		ofile = 'combined.nc'
+	end
+
 	% find netCDF files
 	nc_files = dir([indir '*.nc']);
 	nc_files = {nc_files.name};
@@ -8,15 +17,63 @@ function CCI = combine_netcdf(indir)
 	% number of files (also number of time steps)
 	nfiles = len(nc_files);
 
+
+	% initialize storage structure time axis
+	CCI.tfrac = zeros(nfiles);
+
 	% Set first element flag
 	first = true;
-	% Initialize storage structure
-	% CCI.sst3 = zeros()
+
+	nloc = 0; %this will be updated below
 
 	% loop through netCDF files
 	for tx = 1:nfiles
+		inpath = [indir nc_files{tx}];
+		C = cci_opennc(inpath);
 		if first
 			% Extract spatial metadata and initialze storage structure
-			
+			CCI.lon = C.lon;
+			CCI.lat = C.lat;
+			nlon = length(C.lon);
+			nlat = length(C.lat);
+
+			% Initialize 3-dimensional storage arrays
+			CCI.sst = zeros(nlon, nlat, nfiles);
+			CCI.uncertainty = zeros(nlon, nlat, nfiles);
+			CCI.sea_fraction = zeros(nlon, nlat, nfiles);
+			CCI.sea_ice_fraction = zeros(nlon, nlat, nfiles);
+
 			% turn off first flag
 			first = false;
+		end
+
+		% Store annual data in structure
+		CCI.tfrac(tx) = double(C.year) + (double(C.month) - 1)/12;
+		CCI.sst(:,:,tx) = double(C.sst);
+		CCI.uncertainty(:,:,tx) = double(C.uncertainty);
+		CCI.sea_fraction(:,:,tx) = double(C.sea_fraction);
+		CCI.sea_ice_fraction(:,:,tx) = double(C.sea_ice_fraction);
+	end
+
+	% omit points with >80% sea ice
+	tmp = CCI.sst;
+	tmp(CCI.sea_ice_fraction > .8) = NaN;
+
+	% reshape to 2d
+	nloc = nlon * nlat;
+	cci2d = reshape(tmp, [nloc, nfiles]);
+	cci2d = cci2d';
+
+	% convert to anomaly (1982 - 2010)
+	clim = calc_clim(cci2d(tfrac >= 1982 & tfrac < 2011, :));
+	cci_anom = cci2d - repmat(clim, [ny, 1]);
+
+	% make loc (2d locations)
+	[x,y] = meshgrid(CCI.lat,CCI.lon);
+	loc = [y(:),x(:)];
+
+	% save raw and updated combined CCI data
+	save(opath, 'CCI', 'cci_anom', 'loc')
+end
+
+
