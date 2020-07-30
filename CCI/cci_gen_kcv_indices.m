cacdd64554114265166a4b6f481dd280a3fceb54
advaccaro <avaccaro@usc.edu>
2020-07-30 01:08:06 -0700

    Update KCV indices script

--- CCI/cci_gen_kcv_indices.m
+++ CCI/cci_gen_kcv_indices.m
@@ -15,11 +15,24 @@ C = load(inpath);
 xraw = C.cci_anom;
 [nt,ns] = size(xraw);
 
+% select points with > (1/Kcv)% coverage to prevent failure
+Kcv = 10;
+test = ~isnan(xraw);
+test_sum = sum(test,1);
+index = find(test_sum > (1/Kcv)*nt);
+n_index = length(indx);
+
+xgrid = nan(ntime,n_index);
+for i = 1:nt
+	xgrid(i,:) = xraw(1,index);
+end
+
+
 % get KCV indices
 Kcv = 10;
-[Xcv,cv_in,cv_out] = kcv_indices2d(xraw,Kcv);
+[Xcv,cv_in,cv_out] = kcv_indices2d(xgrid,Kcv);
 
 % Save indices to file
 cv_indices_tag = 'cci_kcv_indices.mat';
 cv_indices_path = [odir cv_indices_tag];
-save(cv_indices_path, 'Xcv', 'cv_in', 'cv_out')
+save(cv_indices_path, 'Xcv', 'cv_in', 'cv_out', 'index')
