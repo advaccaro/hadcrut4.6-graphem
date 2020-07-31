6a29e8f4d1382a1ac1f6ba58e64b22e44795ffb8
advaccaro <avaccaro@usc.edu>
2020-07-30 23:44:00 -0700

    Add C all functions

---
+++ CCI/gen_sbatch_cci_graphem_sp_CV_kn_all.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+spars=$1
+kcv=$2
+fn="SBATCH_CCI_SP$1_K$2_all.txt"
+rn="CCI_SP$1_K$2_A"
+out="cci_graphem_sp$1_k$2_all"
+
+touch $fn
+
+echo "#!/bin/bash" >> $fn
+echo "#SBATCH --job-name=$rn" >> $fn
+echo "#SBATCH --mem=250MB" >> $fn
+echo "#SBATCH --nodelist=equake-03" >> $fn
+echo "cd /home/geovault-02/avaccaro/hadcrut4.6-graphem/CCI" >> $fn
+echo "/usr/bin/matlab -nodesktop -nosplash > ./out/$out.out << EOF" >> $fn
+echo "addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem'));" >> $fn
+echo "target_spars = $1/100;" >> $fn
+echo "Kcv = $2;" >> $fn
+echo "cci_graphem_sp_CV_kn_all(target_spars, Kcv);" >> $fn
+echo "exit" >> $fn
+echo "EOF" >> $fn
