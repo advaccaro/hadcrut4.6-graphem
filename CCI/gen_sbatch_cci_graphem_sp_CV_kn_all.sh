1fb50f563bdbe9c6e04610f625b513307d089af3
advaccaro <avaccaro@usc.edu>
2020-07-30 23:51:07 -0700

    Update matlab name

--- CCI/gen_sbatch_cci_graphem_sp_CV_kn_all.sh
+++ CCI/gen_sbatch_cci_graphem_sp_CV_kn_all.sh
@@ -16,6 +16,6 @@ echo "/usr/bin/matlab -nodesktop -nosplash > ./out/$out.out << EOF" >> $fn
 echo "addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem'));" >> $fn
 echo "target_spars = $1/100;" >> $fn
 echo "Kcv = $2;" >> $fn
-echo "cci_graphem_sp_CV_kn_all(target_spars, Kcv);" >> $fn
+echo "cci_graphem_sp_CV_kn_Call(target_spars, Kcv);" >> $fn
 echo "exit" >> $fn
 echo "EOF" >> $fn
