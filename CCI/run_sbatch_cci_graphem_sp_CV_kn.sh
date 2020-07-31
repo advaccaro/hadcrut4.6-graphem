653445df0a6efd42168e3a9da4a46103bc03197f
advaccaro <avaccaro@usc.edu>
2020-07-30 22:42:29 -0700

    Add sp launch script

---
+++ CCI/run_sbatch_cci_graphem_sp_CV_kn.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+run_sbatch_cci_graphem_sp_CV_kn () {
+	#SBATCH --mem=250MB
+	#SBATCH --job-name=CCI_SP$1_K$2
+	#SBATCH --nodelist=equake-01,equake-02,equake-03,equake-04,equake-05
+	cd /home/geovault-02/avaccaro/hadcrut4.6-graphem/CCI
+	/usr/bin/matlab -nodesktop -nosplash > ./out/CCI_graphem_sp$1_k$2_CV.out << EOF
+	addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem/'));
+	target_spars = $1/100;
+	Kcv = $2;
+	cci_graphem_sp_CV_kn(target_spars, Kcv);
+	exit
+	EOF
+}
