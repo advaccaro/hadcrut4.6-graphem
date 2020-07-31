19072349f591fd80231ac9d4c9c4a693a0cf03ec
advaccaro <avaccaro@usc.edu>
2020-07-30 22:56:35 -0700

    Add sp launch script

--- CCI/run_sbatch_cci_graphem_sp_CV_kn.sh
+++ CCI/run_sbatch_cci_graphem_sp_CV_kn.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-run_sbatch_cci_graphem_sp_CV_kn () {
-	echo "#!/bin/bash
-	#SBATCH --mem=250MB
-	#SBATCH --job-name=CCI_SP$1_K$2
-	#SBATCH --nodelist=equake-01,equake-02,equake-03,equake-04,equake-05
-	cd /home/geovault-02/avaccaro/hadcrut4.6-graphem/CCI
-	/usr/bin/matlab -nodesktop -nosplash > ./out/CCI_graphem_sp$1_k$2_CV.out << EOF
-	addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem/'));
-	target_spars = $1/100;
-	Kcv = $2;
-	cci_graphem_sp_CV_kn(target_spars, Kcv);
-	exit
-	EOF" >> tmp.slurm
-	sbatch tmp.slurm
-	rm tmp.slurm
-}
+spars=$1
+kcv=$2
+fn="SBATCH_CCI_SP$1_K$2.txt"
+rn="CCI_SP$1_K$2"
+out="cci_graphem_sp$1_k$2"
+
+touch $fn
+
+echo "#!/bin/bash" >> $fn
+echo "#SBATCH --jobname=$rn" >> $fn
+echo "#SBATCH --mem=250MB" >> $fn
+echo "#SBATCH --nodelist=equake-02"
+echo "cd /home/geovault-02/avaccaro/hadcrut4.6-graphem/CCI" >> $fn
+echo "/usr/bin/matlab -nodesktop -nosplash > ./out/$out.out << EOF" >> $fn
+echo "addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem'));" >> $fn
+echo "target_spars = $1/100;" >> $fn
+echo "Kcv = $2;" >> $fn
+echo "cci_graphem_sp_CV_kn(target_spars, Kcv);" >> $fn
+echo "exit" >> $fn
+echo "EOF" >> $fn
