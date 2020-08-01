432c5ade57ef6fea147087ba66b8a700abf6a39d
advaccaro <avaccaro@usc.edu>
2020-07-31 17:16:29 -0700

    Add PW calc adj

---
+++ pseudo_world/pseudoworld_calc_adj.m
@@ -0,0 +1,46 @@
+function adjM = pseudoworld_calc_adj(dataset, datatype, worldnum, target_spars)
+	home_dir = '/home/geovault-02/avaccaro/hadcrut4.6-graphem/';
+	addpath(genpath(home_dir))
+	raw = load(dataset)
+	PW = indentifyPW(raw, worldnum, datatype);
+	[nt,ns] = size(PW.grid_2d);
+
+	% select points with > (1/Kcv)% coverage
+	worldname = ['pseudoworld' num2str(worldnum)];
+	fullname = [worldname '_' datatype];
+	basedir = '/home/geovault-02/avaccaro/hadcrut4.6-graphem/pseudo_world/';
+	odir = [basedir worldname '/' datatype '/data/'];
+
+	kcv_indices_tag = [worldname '_' datatype '_kcv_indices.mat'];
+	kcv_indices_path = [odir cv_indices_tag];
+	K = load(kcv_indices_path);
+	index = K.index;
+
+	Xgrid = PW.grid_2d(:,index);
+
+	C0 = corr(Xgrid);
+	greedy_maxit = 50;
+
+	%Sigma_G options
+	sigma_opt.ggm_tol = 5e-3;
+	sigma_opt.ggm_maxit = 200;
+	sigma_opt.ggm_thre = 50;
+	sigma_opt.adj = 0; %placeholder
+
+	[spars, adj] = greedy_search_TT(C0, target_spars, greedy_maxit);
+	spars_f = spars(end);
+	adjM = adj{end};
+
+	% Product well-conditioned C
+	sigma_opt = rmfield(sigma_opt, 'adj');
+	sigma_opt.adj = adjM;
+	[Cw,sp_level] = Sigma_G(C0,sigma_opt);
+
+	% Save adjM and Cw
+	adjtag = [worldnum '_' datatype '_sp' num2str(target_spars*100) '_adj.mat'];
+	adjpath = [odir adjtag];
+	save(adjpath, 'spars_f', 'adjM', 'Cw', 'target_spars')
+end
+
+
+
---
+++ pseudo_world/run_pseudoworld_calc_adj.m
@@ -0,0 +1,24 @@
+home_dir = '/home/geovault-02/avaccaro/hadcrut4.6-graphem/'
+addpath(genpath(home_dir))
+
+datasets = cell(1,8);
+datasets{1} = [home_dir 'pseudoworld1/data/pseudoworld1_sst.mat'];
+datasets{2} = [home_dir 'pseudoworld1/data/pseudoworld1_lsat.mat'];
+datasets{3} = [home_dir 'pseudoworld2/data/pseudoworld2_sst.mat'];
+datasets{4} = [home_dir 'pseudoworld2/data/pseudoworld2_lsat.mat'];
+datasets{5} = [home_dir 'pseudoworld3/data/pseudoworld3_sst.mat'];
+datasets{6} = [home_dir 'pseudoworld3/data/pseudoworld3_lsat.mat'];
+datasets{7} = [home_dir 'pseudoworld4/data/pseudoworld4_sst.mat'];
+datasets{8} = [home_dir 'pseudoworld4/data/pseudoworld4_lsat.mat'];
+
+for i = 1:length(datasets)
+	dataset = datasets{i};
+	pieces = split(dataset, '.mat');
+	pieces2 = split(pieces{1}, '_');
+	datatype = pieces2{end};
+	worldnum = pieces2{end-1}(end);
+
+	for spars = [1.1,1.25,1.5]
+		pseudoworld_calc_adj(dataset, datatype, worldnum, spars);
+	end
+end
