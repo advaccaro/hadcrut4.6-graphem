3bb6c75b09a511564c8b2e0788ece3c316e2c629
advaccaro <avaccaro@usc.edu>
2020-08-02 13:44:41 -0700

    Add CW interpolation

--- cw17/cw_krig.m
+++ cw17/cw_krig.m
@@ -13,8 +13,8 @@ end
 function cov = prepare_cov2(lats, lons, dist) %dist = 1000
 	nlat = length(lats);
 	nlon = length(lons);
-	xs = deg2rad(lons);
-	ys = deg2rad(lats);
+	xs = deg2rad(lats);
+	ys = deg2rad(lons);
 	% xs = deg2rad( ((0:nlat-1)+.5)*180/nlat-90.0);
 	% ys = deg2rad( ((0:nlon-1)+.5)*360/nlon-180.0);
 	nspace = nlat * nlon;
