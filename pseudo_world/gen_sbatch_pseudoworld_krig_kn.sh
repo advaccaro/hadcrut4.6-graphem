4ca6e39e940313d3ec5351acdb1a7d6ebb920bc3
advaccaro <avaccaro@usc.edu>
2020-08-02 17:41:42 -0700

    Fix shell script

--- pseudo_world/gen_sbatch_pseudoworld_krig_kn.sh
+++ pseudo_world/gen_sbatch_pseudoworld_krig_kn.sh
@@ -3,7 +3,7 @@ dist=$1
 worldnum=$2
 datatype=$3
 kcv=$4
-fn="SBATCH_PW$2$3_KRIG$1_K$4_all.txt"
+fn="SBATCH_PW$2$3_KRIG$1_K$4.txt"
 rn="PW$2$3_K$4"
 out="pw$2$3_krig_k$4"
 
@@ -18,7 +18,7 @@ echo "/usr/bin/matlab -nodesktop -nosplash > ./out/$out.out << EOF" >> $fn
 echo "addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem'));" >> $fn
 echo "dist = $1;" >> $fn
 echo "worldnum = $2;" >> $fn
-echo "datatype = $3;" >> $fn
+echo "datatype = '$3';" >> $fn
 echo "Kcv = $4;" >> $fn
 echo "pseudoworld_krig_kn(dist, Kcv, worldnum, datatype);" >> $fn
 echo "exit" >> $fn
