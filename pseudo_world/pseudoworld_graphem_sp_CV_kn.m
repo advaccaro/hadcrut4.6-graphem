5e7cbab931b71c0314b68753b509a2b18032cc3f
advaccaro <avaccaro@usc.edu>
2020-08-15 18:41:32 -0700

    Fix errors

--- pseudo_world/pseudoworld_graphem_sp_CV_kn.m
+++ pseudo_world/pseudoworld_graphem_sp_CV_kn.m
@@ -1,6 +1,6 @@
 %pseudoworld_graphem_sp_CV.m
 function pseudoworld_graphem_sp_CV_kn(dataset,datatype,worldnum,target_spars,Kcv)
-
+	k = Kcv;
 	% Cross-validation of target sparsity for GraphEM w/ GLASSO
 	tic
 
@@ -23,9 +23,28 @@ function pseudoworld_graphem_sp_CV_kn(dataset,datatype,worldnum,target_spars,Kcv
 	K = load(kcv_indices_path);
 
 	% Load adjacency matrix and well-conditioned C
-	adjtag = [fullname '_sp' num2str(target_spars*100) '_adj.mat'];
-	adjpath = [odir adjtag];
-	load(adjpath)
+	index = K.index;
+
+
+	Xgrid = PW.grid_2d(:,index);
+
+	C0 = corr(Xcv{k});
+	greedy_maxit = 50;
+
+	%Sigma_G options
+	sigma_opt.ggm_tol = 5e-3;
+	sigma_opt.ggm_maxit = 200;
+	sigma_opt.ggm_thre = 50;
+	sigma_opt.adj = 0; %placeholder
+
+	[spars, adj] = greedy_search_TT(C0, target_spars, greedy_maxit);
+	spars_f = spars(end);
+	adjM = adj{end};
+
+	% Product well-conditioned C
+	sigma_opt = rmfield(sigma_opt, 'adj');
+	sigma_opt.adj = adjM;
+	[Cw,sp_level] = Sigma_G(C0,sigma_opt);
 
 
 	% GraphEM options
@@ -42,11 +61,10 @@ function pseudoworld_graphem_sp_CV_kn(dataset,datatype,worldnum,target_spars,Kcv
 		% Run GraphEM
 		[Xg,Mg,Cg] = graphem(double(Xcv{k}),opt);
 		Xg_k = Xg;
-		save(SPkfoldpath, 'Xg_k', 'target_spars', 'adjM', 'index')
+		save(SPkfoldpath, 'Xg_k', 'target_spars', 'adjM')
 		% else
 		% 	load(SPkfoldpath)
 		% 	Xg{k} = Xg_k;
-		end
 		clear Xg_k
-	end
+		end
 end
