e9baabc5145480e30c2b7f3477bf501531cefa48
advaccaro <avaccaro@usc.edu>
2020-08-22 09:54:40 -0700

    Use iridium, change slurm name to *KCV

--- pseudo_world/gen_sbatch_pseudoworld_cr_cv_full.m
+++ pseudo_world/gen_sbatch_pseudoworld_cr_cv_full.m
@@ -3,8 +3,8 @@ dist=$1
 worldnum=$2
 datatype=$3
 kcv=$4
-fn="SBATCH_PW$2$3_CR$1_K$4.txt"
-rn="PW$2$3_K$4"
+fn="SBATCH_PW$2$3_CR$1_KCV.txt"
+rn="PW$2$3_KCV"
 out="pw$2$3_CR_k$4"
 
 touch $fn
@@ -12,7 +12,7 @@ touch $fn
 echo "#!/bin/bash" >> $fn
 echo "#SBATCH --job-name=$rn" >> $fn
 echo "#SBATCH --mem=4GB" >> $fn
-echo "#SBATCH --nodelist=equake-01" >> $fn
+echo "#SBATCH --nodelist=iridium" >> $fn
 echo "cd /home/geovault-02/avaccaro/hadcrut4.6-graphem/" >> $fn
 echo "/usr/bin/matlab -nodesktop -nosplash > ./out/$out.out << EOF" >> $fn
 echo "addpath(genpath('/home/geovault-02/avaccaro/hadcrut4.6-graphem'));" >> $fn
